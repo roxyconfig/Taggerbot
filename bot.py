72a16c5974976fd90c3dd833997f6f147aadd200
sekret666 <66795778+sekret666@users.noreply.github.com>
2022-01-04 10:28:27 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,8 +17,11 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 anlik_calisan = []
 aykhan_tag = []
-
-
+#tektag
+@client.on(events.NewMessage(pattern='^(?i)/cancel'))
+async def cancel(event):
+  global aykhan_tag
+  aykhan_tag.remove(event.chat_id)
 
 @client.on(events.NewMessage(pattern='^(?i)/cancel'))
 async def cancel(event):
